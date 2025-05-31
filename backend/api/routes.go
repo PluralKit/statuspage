9fbebd98e8007f9a3539a3d546aa6352e986c300
asleepyskye <skye@vixen.lgbt>
2025-05-30 21:18:14 -0400

    fix: move edit routes to behind admin route

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -26,33 +26,37 @@ func NewAPI(config util.Config, logger *slog.Logger, database *db.DB) *API {
 func (a *API) SetupRoutes(router *chi.Mux) {
 	router.Route("/api/v1", func(r chi.Router) {
 
+		r.Get("/status", a.GetStatus)
+
 		r.Route("/incidents", func(r chi.Router) {
 			r.Get("/", a.GetIncidents)
 			r.Get("/active", a.GetActiveIncidents)
-			r.Post("/create", a.CreateIncident)
-
 			r.Route("/{incidentID}", func(r chi.Router) {
 				r.Get("/", a.GetIncident)
-				r.Patch("/", a.EditIncident)
-				r.Delete("/", a.DeleteIncident)
-
 				r.Route("/update", func(r chi.Router) {
-					r.Post("/", a.AddUpdate)
 					r.Route("/{updateID}", func(r chi.Router) {
 						r.Get("/", a.GetUpdate)
-						r.Patch("/", a.EditUpdate)
-						r.Delete("/", a.DeleteUpdate)
 					})
 				})
-				/*
-					TODO: add a resolve route
-					or a more general status update route?
-					tryin to keep it simple, but more routes might be the way to go here
-					we need to be able to auto set resolution time somehow ?
-				*/
 			})
 		})
 
-		r.Get("/status", a.GetStatus)
+		r.Route("/admin", func(r chi.Router) {
+			r.Route("/incidents", func(r chi.Router) {
+				r.Post("/create", a.CreateIncident)
+				r.Route("/{incidentID}", func(r chi.Router) {
+					r.Patch("/", a.EditIncident)
+					r.Delete("/", a.DeleteIncident)
+					r.Route("/update", func(r chi.Router) {
+						r.Post("/", a.AddUpdate)
+						r.Route("/{updateID}", func(r chi.Router) {
+							r.Patch("/", a.EditUpdate)
+							r.Delete("/", a.DeleteUpdate)
+						})
+					})
+				})
+			})
+		})
+
 	})
 }
